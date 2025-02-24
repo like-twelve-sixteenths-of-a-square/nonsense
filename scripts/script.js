eae34ccf10dd46873ef9d2951fc89b59d921590e
like-twelve-sixteenths-of-a-square <enzo.ressa@angrykitten.com>
2025-02-24 00:12:30 -0800

    this craphole

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+//Hamburbur menu Function
+function hamburger() {
+	var menu = document.getElementById("menu-links");
+	var logo = document.getElementById("ffc-logo");
+	if (menu.style.display === "block" && logo.style.display === "none") {
+		menu.style.display = "none";
+		logo.style.display = "block";
+	} else {
+		menu.style.display = "block";
+		logo.style.display = "none";
+	}
+}
