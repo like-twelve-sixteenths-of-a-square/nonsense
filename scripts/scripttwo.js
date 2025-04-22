bbe6dafccae4b371c5c3a23d1469876d2db43071
like-twelve-sixteenths-of-a-square <enzo.ressa@angrykitten.com>
2025-04-22 04:43:21 -0700

    okay this is really funny now

--- scripts/scripttwo.js
+++
@@ -1,7 +0,0 @@
-//Something close enough I hope
-function updateMessage() {
-	var msg= 'Want to feature a feature creature for your own feature creature creature feature? Dial 555-255-5255-522-222-552-CREATURE-52-5555-52555 to buy your own for ABSOLUTELY FREE!! Only $499.81!'
-	var el = document.getElementById('offer')
-	el.textContent = msg;
-}
-updateMessage();
