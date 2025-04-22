6872daaa14dfb16498dcb4c53176f8c73187b4eb
like-twelve-sixteenths-of-a-square <enzo.ressa@angrykitten.com>
2025-04-22 15:52:19 -0700

    Back to basics, let's make it work this time

---
+++ scripts/scriptthree.js
@@ -0,0 +1,9 @@
+//Let's try adding this to cover the bases
+	var first = 'Act fast! We only have ';
+	var remaining = Math.floor(Math.random()*(299-100)*100);
+	var last =' left, and they are going fast fast fast!';
+	
+	var chatterMessage = first + remaining + last;
+	
+	var el = document.getElementById('countit');
+	el.textContent = chatterMessage;
