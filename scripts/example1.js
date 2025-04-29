638621a381f0ea4b7ad46a27ae432f2492a97ba7
like-twelve-sixteenths-of-a-square <enzo.ressa@angrykitten.com>
2025-04-29 00:05:06 -0700

    Notation
    Actually decided to add comments to the script so its intelligible, look at me being mature.

--- scripts/example1.js
+++ scripts/example1.js
@@ -1,22 +1,22 @@
-var operator = 'chatter'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var txtRng = Math.floor(Math.random()*25) + 2;
+var operator = 'chatter'; // Obligatory operator thing
+var i = 1;                // Set counter to 1
+var txtRng = Math.floor(Math.random()*25) + 2; //Pick a random number from 2 to 24, I believe
 var msg = '';              // Message
 
 if (operator === 'chatter') {
-  // Do addition
+  // Flip a virtual coin, if heads, say Creature, if tails, say Feature, repeat that a randomized number of times
   while (i < txtRng) {
-	var blabber = Math.floor(Math.random()*2)+1;
+	var blabber = Math.floor(Math.random()*2)+1; //Virtual coin flip, literally just pick a random number between 1 and 2.
 		if (blabber < 2) {
-			msg += 'Creature ';
+			msg += 'Creature '; //If 1, say Creature
 			i++;	
 		} else {
-			msg += 'Feature ';
+			msg += 'Feature '; //If not 1, say Feature
 			i++;
 		}
   }
 }
 
-// Write the message into the page
+// Spit out the randomized string of glorious nonsense
 var el = document.getElementById('blabber');
 el.innerHTML = msg;
