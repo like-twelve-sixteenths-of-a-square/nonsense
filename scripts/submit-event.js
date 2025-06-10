19a818f44e5a8c532b498548b44b5f9c09fec709
like-twelve-sixteenths-of-a-square <enzo.ressa@angrykitten.com>
2025-06-10 00:59:45 -0700

    tweak

--- scripts/submit-event.js
+++ scripts/submit-event.js
@@ -5,7 +5,7 @@
     e.preventDefault();                              // Stop it being sent
     var elements = this.elements;                    // Get all form elements
     var username = elements.username.value;          // Select username entered
-    var msg      = 'Alright, alright, you are not on our records but we are gonna let this slide, not like you can try anything anyway. Welcome, ' + username + '. Look around, but do NOT touch anything, the paint is still wet.';            // Create welcome message
+    var msg      = 'Yeah, you are not supposed to be here. Whatever, welcome, ' + username + '. Look around, but do NOT touch anything, the paint is still wet.';            // Create welcome message
     document.getElementById('main').textContent = msg; // Write welcome message
   });
 }());
